84520ad2b3eea99825f72d1e7639b522adbd6bee
nuno <nm.correia@campus.fct.unl.pt>
2014-12-24 04:21:38 +0000

    Fix mount script

--- adfs.sh
+++ adfs.sh
@@ -1,4 +1,3 @@
 # Script to mount ADFS
-
 LD_LIBRARY_PATH=/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server:bin/
-java -cp "lib/*:conf/*" -Dlog4j.configuration=file:conf/log4j.properties -Djava.library.path=$LD_LIBRARY_PATH adfs.client.ADFSClient $*
+java -Djava.library.path=$LD_LIBRARY_PATH -classpath lib/*:conf: adfs.client.ADFSClient -f adfs $*
